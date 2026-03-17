d1c4a02ca86a1fe0439fa69ed70f546eedc10a1d
Jordan Thomas <jordanthomas@Jordans-MacBook-Pro.local>
2026-03-17 15:07:40 +0800

    initial commit testing GHA AWS deployment with terraform and VPC

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project}-${var.environment}"
+  cidr = var.vpc_cidr
+
+  azs             = var.availability_zones
+  public_subnets  = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
+
+  # Single NAT GW is sufficient for sandbox; set one_nat_gateway_per_az = true for prod
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # EKS requires these tags for the AWS Load Balancer Controller to
+  # auto-discover subnets when provisioning load balancers
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "nat_gateway_public_ips" {
+  description = "Public IPs of the NAT gateway(s)"
+  value       = module.vpc.nat_public_ips
+}
